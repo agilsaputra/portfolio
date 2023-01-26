e0dee88ad65d7172e0eae13c9e3c726ea8f8a887
agilsaputra <m.agil.saputra@gmail.com>
2023-01-26 18:36:31 +0700

    Update theme.js

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 const ThemeContext = createContext()
