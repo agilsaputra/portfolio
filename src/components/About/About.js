4e2d7e77e641b1139fa80c5eccf70edcff0ff7ba
agilsaputra <m.agil.saputra@gmail.com>
2023-04-16 10:13:50 +0000

    a

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,7 +15,7 @@ const About = () => {
       )}
 
       {role && <h2 className='about__role'>A {role}.</h2>}
-      
+      <p className='about__desc'>{description && description}</p>
 
       <div className='about__contact center'>
         {resume && (
