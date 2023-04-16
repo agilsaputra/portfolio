ddff3ee563e04596a51cfd3d37ebb674a4b0a8ee
agilsaputra <m.agil.saputra@gmail.com>
2023-04-16 09:53:37 +0000

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,11 +9,11 @@ const about = {
   name: 'M Agil Saputra',
   role: '',
   description:
-    'Currently working as Ebay Admin In addition to my eBay Admin duties, I also manage web maintenance tasks for https://pa6autoparts.com. This includes regular website updates, such as product additions, pricing updates, and content modifications, to ensure the site remains current and user-friendly. I also conduct routine checks for website functionality, including monitoring for broken links, resolving technical issues, and optimizing site speed and performance.',
+    'Currently working as Ebay Admin In addition to my eBay Admin duties, I also manage web maintenance tasks for<a href="https://pa6autoparts.com" target="_blank">https://pa6autoparts.com</a>. This includes regular website updates, such as product additions, pricing updates, and content modifications, to ensure the site remains current and user-friendly. I also conduct routine checks for website functionality, including monitoring for broken links, resolving technical issues, and optimizing site speed and performance.',
   resume: 'https://example.com',
   social: {
     linkedin: 'https://linkedin.com',
-    github: 'https://github.com/agilsaputra',
+    github: 'https://github.com/agisaputra',
   },
 }
 
@@ -21,9 +21,16 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Extract data from twitter API using python',
+    name: 'Admin for Ebay Seller',
     description:
-      'Simple python script to pull data from twitter API then store on csv file, this script will pull tweet contain words "indonesia" and location in yogyakarta',
+      `Februari 2021 - Present (2 tahun)
+
+      - order management : i'm manage order from receiving purchase orders to ensuring timely and accurate order processing
+      - handle buyer around the world  : 
+      ensured compliance and resolved any order-related issues e.g : international shipping regulation, tax regulation each country 
+      - manage stock inventory : managed stock inventory to ensure optimal stock availability and avoid stockouts .
+      - Create Daily Inventory Report for Stock In and Stock Out
+      - Manage customer returns according to term and polices`,
     stack: ['Python', 'TwitterAPI', 'Tweepy','Parameters base Standard search API'],
     sourceCode: 'https://github.com/agilsaputra/python-script-to-csv',
     livePreview: 'https://github.com/agilsaputra/python-script-to-csv/blob/master/tweets.csv',
