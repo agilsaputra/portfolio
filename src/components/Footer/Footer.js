b115478ebc1c07fcd94ce35882f3552368fc41ea
agilsaputra <m.agil.saputra@gmail.com>
2023-04-17 22:36:26 +0700

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://agilsaputra.netlify.app/'
+      href='https://pa6autoparts.com/'
       className='link footer__link'
     >
-      Currently learn Database Enggineer 
+      Visit website 
     </a>
   </footer>
 )
