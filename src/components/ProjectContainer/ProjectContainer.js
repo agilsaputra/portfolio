5af21bd0f848fb9cf9cabab21d86ac0180c3c404
agilsaputra <m.agil.saputra@gmail.com>
2023-04-17 17:32:55 +0700

    Update ProjectContainer.js

--- src/components/ProjectContainer/ProjectContainer.js
+++ src/components/ProjectContainer/ProjectContainer.js
@@ -7,14 +7,16 @@ const ProjectContainer = ({ project }) => (
   <div className='project'>
     <h3>{project.name}</h3>
 
-  <p className='project__description'>
-  {project.description.split('\n').map((line, index) => (
-    <React.Fragment key={index}>
-      {line}
-      <br />
-    </React.Fragment>
-  ))}
-</p>
+  <p className='project__description'>{project.description}</p>
+    {project.stack && (
+      <ul className='project__stack'>
+        {project.stack.map((item) => (
+          <li key={uniqid()} className='project__stack-item'>
+            {item}
+          </li>
+        ))}
+      </ul>
+    )}
 
     {project.sourceCode && (
       <a
