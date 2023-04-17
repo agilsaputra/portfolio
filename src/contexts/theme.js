396b79f9c5a7c7a0b233ba52502175ef99f5f660
agilsaputra <m.agil.saputra@gmail.com>
2023-04-17 11:56:14 +0000

    change dark

--- src/contexts/theme.js
+++ src/contexts/theme.js
@@ -1,34 +1,37 @@
-import React, { createContext, useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
+import React, { createContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-const ThemeContext = createContext()
+const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
   const [themeName, setThemeName] = useState('dark');
 
   useEffect(() => {
     const darkMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-    setThemeName(darkMediaQuery.matches ? 'dark' : 'light')
-    darkMediaQuery.addEventListener('change', (e) => {
-      setThemeName(e.matches ? 'dark' : 'light')
-    });
-  }, [])
+    setThemeName('dark'); // Set themeName directly to 'dark'
+    // Remove event listener for 'change' on darkMediaQuery
+
+    return () => {
+      // Clean up by removing event listener when component unmounts
+      darkMediaQuery.removeEventListener('change');
+    };
+  }, []);
 
   const toggleTheme = () => {
-    const name = themeName === 'dark' ? 'dark' : 'light'
-    localStorage.setItem('themeName', name)
-    setThemeName(name)
-  }
+    const name = themeName === 'dark' ? 'dark' : 'light';
+    localStorage.setItem('themeName', name);
+    setThemeName(name);
+  };
 
   return (
     <ThemeContext.Provider value={[{ themeName, toggleTheme }]}>
       {children}
     </ThemeContext.Provider>
-  )
-}
+  );
+};
 
 ThemeProvider.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export { ThemeProvider, ThemeContext }
+export { ThemeProvider, ThemeContext };
